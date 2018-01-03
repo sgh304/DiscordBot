60b45165d186f3f628474e44e76756038e45e668
WesLSmith <32778563+WesLSmith@users.noreply.github.com>
2018-01-02 19:43:23 -0500

    Add files via upload

--- critmas_bot.py
+++ critmas_bot.py
@@ -84,34 +84,34 @@ async def bans():
 
 ##Get the top 5 counters for a given champion
 @bot.command()
-async def counters(champion):
-    htmldoc = urllib.request.urlopen("http://na.op.gg/champion/" + champion + "/statistics/top/matchup").read()
-    soup = BeautifulSoup(htmldoc)
+async def counters(champion, lane = None):
+    if lane is None:
+        await bot.say("Please choose a lane! Try something like this: \"?counters Jhin bot\"")
+    else:
+        htmldoc = urllib.request.urlopen("http://na.op.gg/champion/" + champion + "/statistics/" + lane + "/matchups").read()
+        soup = BeautifulSoup(htmldoc)
 
-    ##Get champion names
-    name_divs = soup.findAll("ul", {"class" : "champion-matchup-champion-list"})
-    counters = re.findall(r"bc'&gt;(.*?)&lt;", str(name_divs))
-    print(counters)
+        ##Get champion names
+        name_divs = soup.find_all("div", {"class" : "champion-matchup-list__champion"})
+        counters = re.findall(r"<span>(.*?)</span>", str(name_divs))
+        win_rates = re.findall(r"([0123456789\.]{1,5})(?=%)", str(name_divs))
 
-    ##Get win rates
-    win_rate_divs = soup.findAll('td', {"class" :"champion-matchup-champion-list__winrate"})
-    win_rates = []
-    for i in win_rate_divs:
-        win_rates.append(str(i)[-11:-5])
-    print(win_rates)
-    
-    # Display top five counters and their win rates
-    await bot.say("The top five %s counters are: (op.gg)" % champion)
-    message1 = "Best Counter: " + counters[0] + " | Win Rate: " + win_rates[0]
-    message2 = "Second: " + counters[1] + " | Win Rate: " + win_rates[1]
-    message3 = "Third: " + counters[2] + " | Win Rate: " + win_rates[2]
-    message4 = "Fourth: " + counters[3] + " | Win Rate: " + win_rates[3]
-    message5 = "Fifth: " + counters[4] + " | Win Rate: " + win_rates[4]
-    
-    await bot.say(message1)
-    await bot.say(message2)
-    await bot.say(message3)
-    await bot.say(message4)
-    await bot.say(message5)
-    
+        win_rates, counters = zip(*sorted(zip(win_rates, counters)))
+
+
+        # Display top five counters and their win rates
+
+        message1 = "Best Counter: " + counters[0] + " | " + champion + " Win Rate: " + win_rates[0] + "%"
+        message2 = "Second: " + counters[1] + " | " + champion + " Win Rate: " + win_rates[1] + "%"
+        message3 = "Third: " + counters[2] + " | " + champion + " Win Rate: " + win_rates[2] + "%"
+        message4 = "Fourth: " + counters[3] + " | " + champion + " Win Rate: " + win_rates[3] + "%"
+        message5 = "Fifth: " + counters[4] + " | " + champion + " Win Rate: " + win_rates[4] + "%"
+
+        await bot.say("The top five %s %s counters are: (op.gg)" % (champion,lane))
+        await bot.say(message1)
+        await bot.say(message2)
+        await bot.say(message3)
+        await bot.say(message4)
+        await bot.say(message5)
+
 bot.run("Mzk0MjcxMjIxNjc3Njg2Nzk0.DSxz6A.Rj5IaLDsiEPwMQ2nX1GW6XL7_ZY")
